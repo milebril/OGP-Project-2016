bbcccb42642566d8d44ff8582046efd83a023f4d
Sjaan Vandebeek <sjaan.vandebeek@student.kuleuven.be>
2016-03-11 23:30:35 +0100

    boulder en log (position + being carried)
    eerst moe unit en world worden aangepast. dan kan boulder + log worden
    afgemaakt. ik heb in deze classes TODO's  toegevoegd aan wat er nog moet
    gebeuren

--- OGP1516-Hillbillies/src/hillbillies/model/Boulder.java
+++ OGP1516-Hillbillies/src/hillbillies/model/Boulder.java
@@ -1,6 +1,7 @@
 package hillbillies.model;
 
 import be.kuleuven.cs.som.annotate.*;
+import java.util.Random;
 
 /**
  * A class that deals with a boulder and all the properties of the boulder 
@@ -14,6 +15,7 @@ import be.kuleuven.cs.som.annotate.*;
  */
 public class Boulder {
 
+////////////////////////////////////////////Constructor////////////////////////////////////////////
 	/**
 	 * Initialize this new Boulder with given weight.
 	 * 
@@ -27,14 +29,17 @@ public class Boulder {
 	 *       | ! canHaveAsWeight(this.getWeight())
 	 * @throws ExceptionName_Java
 	 */
-	public Boulder(int weight) throws IllegalArgumentException {
+	public Boulder(double[] position) throws IllegalArgumentException {
+		Random random = new Random();
+		int weight = random.nextInt(40) + 10;
 		if (! canHaveAsWeight(weight))
 			throw new IllegalArgumentException();
-		this.weight = weight;
-		//TODO constructor
+		this.weight =  weight;
+		//TODO placed on passable cube?
 	}
  
-	
+////////////////////////////////////////////Weight////////////////////////////////////////////
+
 	/**
 	 * Return the weight of this Boulder.
 	 */
@@ -76,4 +81,134 @@ public class Boulder {
 	 * Variable registering the weight of this Boulder.
 	 */
 	private final int weight;
+
+////////////////////////////////////////////Position////////////////////////////////////////////	
+
+	/**
+	 * Return the position of this boulder.
+	 */
+	@Basic @Raw
+	public double[] getPosition() {
+		return this.position;
+	}
+	
+	/**
+	 * Check whether the given position is a valid position for
+	 * any boulder.
+	 *  
+	 * @param  position
+	 *         The position to check.
+	 * @return if all the three elements of the list lays between the minimum
+	 * 		   and maximum value for a coordinate return true. 
+	*/
+	public static boolean isValidPosition(double[] position) {
+		if (position.length > 3)
+			return false;
+		for (int i=0; i < position.length; i++) {
+			if (position[i] < getMinValueCoordinate() || position[i] > getMaxValueCoordinate())
+				return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * Set the position of this boulder to the given position.
+	 * 
+	 * @param  boulder
+	 *         The new position for this boulder.
+	 * @post   The position of this new log is equal to
+	 *         the given position.
+	 *       | new.getPosition() == boulder
+	 * @throws illegalArgumentException
+	 *         The given position is not a valid position for any
+	 *         boulder.
+	 *       | ! isValidPosition(getPosition())
+	 */
+	@Raw
+	public void setPosition(double[] position) throws IllegalArgumentException {
+		if (! isValidPosition(position))
+			throw new IllegalArgumentException();
+		this.position = position;
+	}
+	
+	/**
+	 * cast an array of integers in an array of doubles
+	 * @param arrayInInt
+	 * @return arrayInDouble
+	 */
+	private double[] castIntToDouble (int[] arrayInInt){
+		double[] arrayInDouble = {(double) arrayInInt[0],(double) arrayInInt[1],(double) arrayInInt[2]};
+		return arrayInDouble;
+	}
+	
+	/**
+	 * cast an array of doubles in an array of integers
+	 * @param arrayInDouble
+	 * @return arrayInInt
+	 */
+	private int[] castDoubleToInt (double[] arrayInDoubles){
+		int[] arrayInInt = {(int) arrayInDoubles[0],(int) arrayInDoubles[1],(int) arrayInDoubles[2]};
+		return arrayInInt;
+	}
+	
+	/**
+	 * Return the value of the lowest coordinate value.
+	 */
+	private static int getMinValueCoordinate(){
+		return 0;
+	}
+	
+	/**
+	 * Return the value of the lowest coordinate value.
+	 */
+	private static int getMaxValueCoordinate(){
+		return 49;
+	}
+	
+	/**
+	 * Variable registering the position of this boulder.
+	 */
+	private double[] position;
+////////////////////////////////////////////Advance Time////////////////////////////////////////////	
+	
+	/**
+	 * 
+	 */
+	public void advanceTime(double dt){
+		//TODO advance time
+	}
+////////////////////////////////////////////Carried////////////////////////////////////////////
+	
+	/**
+	 * Make a boulder start being carried.
+	 */
+	public void startBeingCarried() throws IllegalStateException{
+		if (this.isCarried == true)
+			throw new IllegalStateException();
+		this.isCarried = true;
+	}
+	
+	/**
+	 * Make a boulder stop being carried.
+	 */
+	public void stopBeingCarried(){
+		this.isCarried = false;
+	}
+	
+	/**
+	 * Boolean registering if a boulder is carried or not.
+	 */
+	private boolean isCarried = false; 
+	
+	//TODO add weight to unit
+	//TODO move boulder witch same vector as unit
+	
+////////////////////////////////////////////Falling////////////////////////////////////////////	
+	
+	//TODO faling
+	
+////////////////////////////////////////////Dropped////////////////////////////////////////////	
+
+	//TODO being dropped
+	
 }
--- OGP1516-Hillbillies/src/hillbillies/model/Log.java
+++ OGP1516-Hillbillies/src/hillbillies/model/Log.java
@@ -1,6 +1,7 @@
 package hillbillies.model;
 
 import be.kuleuven.cs.som.annotate.*;
+import java.util.Random;
 
 /**
  * A class that deals with logs and all the properties of the logs
@@ -8,11 +9,15 @@ import be.kuleuven.cs.som.annotate.*;
  * 
  * @invar  Each log can have its weight as weight.
  *       | canHaveAsWeight(this.getWeight())
+ * @invar  The position of each log must be a valid position for any
+ *         log.
+ *       | isValidPosition(getPosition())
  * @author Emil Peters
  * @author Sjaan Vandebeek
  */
 public class Log {
 	
+////////////////////////////////////////////Constructor////////////////////////////////////////////	
 	/**
 	 * Initialize this new log with given weight.
 	 * 
@@ -24,14 +29,25 @@ public class Log {
 	 * @throws IllegalArgumentException
 	 *         This new log cannot have the given weight as its weight.
 	 *       | ! canHaveAsWeight(this.getWeight())
+	 *       
+	 * Initialize this new log with given position.
+	 *
+	 * @param  position
+	 *         The position for this new log.
+	 * @effect The position of this new log is set to
+	 *         the given position.
+	 *       | this.setPosition(position)
 	 */
-	public Log(int weight) throws IllegalArgumentException {
+	public Log(double[] position) throws IllegalArgumentException {
+		Random random = new Random();
+		int weight = random.nextInt(40) + 10;
 		if (! canHaveAsWeight(weight))
 			throw new IllegalArgumentException();
-		this.weight = weight;
-		//TODO constructor
+		this.weight =  weight;
+		//TODO placed on passable cube?
 	}
  
+////////////////////////////////////////////Weight////////////////////////////////////////////
 	
 	/**
 	 * Return the weight of this log.
@@ -74,4 +90,136 @@ public class Log {
 	 * Variable registering the weight of this log.
 	 */
 	private final int weight;
+	
+////////////////////////////////////////////Position////////////////////////////////////////////	
+	
+	/**
+	 * Return the position of this log.
+	 */
+	@Basic @Raw
+	public double[] getPosition() {
+		return this.position;
+	}
+	
+	/**
+	 * Check whether the given position is a valid position for
+	 * any log.
+	 *  
+	 * @param  position
+	 *         The position to check.
+	 * @return if all the three elements of the list lays between the minimum
+	 * 		   and maximum value for a coordinate return true.
+	 *       
+	*/
+	public static boolean isValidPosition(double[] position) {
+		if (position.length > 3)
+			return false;
+		for (int i=0; i < position.length; i++) {
+			if (position[i] < getMinValueCoordinate() || position[i] > getMaxValueCoordinate())
+				return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * Set the position of this log to the given position.
+	 * 
+	 * @param  position
+	 *         The new position for this log.
+	 * @post   The position of this new log is equal to
+	 *         the given position.
+	 *       | new.getPosition() == position
+	 * @throws IllegalArgumentException
+	 *         The given position is not a valid position for any
+	 *         log.
+	 *       | ! isValidPosition(getPosition())
+	 */
+	@Raw
+	public void setPosition(double[] position) throws IllegalArgumentException {
+		if (! isValidPosition(position))
+			throw new IllegalArgumentException();
+		this.position = position;
+	}
+	
+	/**
+	 * cast an array of integers in an array of doubles
+	 * @param arrayInInt
+	 * @return arrayInDouble
+	 */
+	private double[] castIntToDouble (int[] arrayInInt){
+		double[] arrayInDouble = {(double) arrayInInt[0],(double) arrayInInt[1],(double) arrayInInt[2]};
+		return arrayInDouble;
+	}
+	
+	/**
+	 * cast an array of doubles in an array of integers
+	 * @param arrayInDouble
+	 * @return arrayInInt
+	 */
+	private int[] castDoubleToInt (double[] arrayInDoubles){
+		int[] arrayInInt = {(int) arrayInDoubles[0],(int) arrayInDoubles[1],(int) arrayInDoubles[2]};
+		return arrayInInt;
+	}
+	
+	/**
+	 * Return the value of the lowest coordinate value.
+	 */
+	private static int getMinValueCoordinate(){
+		return 0;
+	}
+	
+	/**
+	 * Return the value of the lowest coordinate value.
+	 */
+	private static int getMaxValueCoordinate(){
+		return 49;
+	}
+	
+	/**
+	 * Variable registering the position of this log.
+	 */
+	private double[] position;
+////////////////////////////////////////////Advance Time////////////////////////////////////////////	
+	
+	/**
+	 * 
+	 */
+	public void advanceTime(double dt){
+		//TODO advance time
+	}
+	
+////////////////////////////////////////////Carried////////////////////////////////////////////
+	
+	/**
+	 * Make a log start being carried.
+	 */
+	public void startBeingCarried() throws IllegalStateException{
+		if (this.isCarried == true)
+			throw new IllegalStateException();
+		this.isCarried = true;
+	}
+	
+	/**
+	 * Make a log stop being carried.
+	 */
+	public void stopBeingCarried(){
+		this.isCarried = false;
+	}
+	
+	/**
+	 * Boolean registering if a log is carried or not.
+	 */
+	private boolean isCarried = false; 
+	
+	//TODO add weight to unit
+	//TODO move boulder witch same vector as unit
+	
+////////////////////////////////////////////Falling////////////////////////////////////////////	
+	
+	//TODO faling
+	
+////////////////////////////////////////////Dropped////////////////////////////////////////////	
+	
+	//TODO being dropped
+	
 }
